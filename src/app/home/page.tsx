39613e4714e89646f3cf918c5c5d0e0ef2de322f
Vishal Kumar Gupta <vishal.gupta@konsentus.com>
2023-03-23 21:53:27 +0000

    chore: cleanup code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,4 @@
-import { useRouter } from 'next/navigation';
-import { useSupabase } from '../../components/supabase-provider';
-
 export default function Home() {
-  const router = useRouter();
-  const { supabase } = useSupabase();
-
-  const handleLogout = async () => {
-    const { error } = await supabase.auth.signOut();
-
-    if (error) {
-      console.log({ error });
-    } else {
-      router.push('/');
-    }
-  };
   return (
     <>
       <div className="flex h-screen w-screen items-center justify-center">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import { useSupabase } from '../components/supabase-provider';
 import Home from './home/page';
 import Login from './login/page';
 
 export default function Page() {
-  const router = useRouter();
-  const { supabase, session } = useSupabase();
-
-  const handleLogout = async () => {
-    const { error } = await supabase.auth.signOut();
-
-    if (error) {
-      console.log({ error });
-    } else {
-      router.push('/');
-    }
-  };
+  const { session } = useSupabase();
 
   return session ? <Home /> : <Login />;
 }
